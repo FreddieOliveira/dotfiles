6120627b2fee1d15cedf668a498925b707f8632e
FreddieOliveira <fredf_oliveira@ufu.br>
2021-02-13 02:15:43 -0300

    install.sh: add new option to install plugins

--- install.sh
+++ install.sh
@@ -17,16 +17,17 @@ general_usage() {
   printf "\nCommands:\n"
   printf "    install\tInstall specified dotfiles\n"
   printf "    list\tList available dotfiles and its files\n"
-  printf "    tui\t\tStart setup wizard ncurse based TUI\n"
+  printf "    tui\t\tStart setup wizard ncurses/newt based TUI\n"
   printf "\nRun '%s <command> -h' for more information on a command.\n" \
     "${0##*/}"
 }
 
 install_usage() {
-  printf "Usage:\t%s install [-es] <dotfile,...>\n" "${0##*/}"
+  printf "Usage:\t%s install [-eps] <dotfile,...>\n" "${0##*/}"
   printf "\nInstall specified dotfiles by comma separated list. If no dotfiles\nare specified, then install all of them. Alternatively, install all\ndotfiles, but the specified ones.\n"
   printf "\nArguments:\n"
-  printf "  -e, --exclude <dotfile,...>\tInstall all dotfiles, exept the comma\n\t\t\t\tseparated listed ones\n"
+  printf "  -e, --exclude <dotfile,...>\tInstall all dotfiles, exept the comma\n\t\t\t\tseparated listed ones\n\n"
+  printf "  -p, --plugins\t\t\tInstall plugins for selected dotfiles. If\n\t\t\t\tno dotfile was especified, then install all\n\t\t\t\tplugins. Available ones are zsh (fast-syntax\n\t\t\t\t-highlighting, fzf-tab, zsh-autosuggestions)\n\t\t\t\tnvim (see init.vim file) and tmux (tpm,\n\t\t\t\ttmux-resurrect, tmux-continuum)\n\n"
   printf "  -s, --symlink\t\t\tInstall the dotfiles as symlinks instead of\n\t\t\t\tcopying them. This is useful to keep using\n\t\t\t\tthis folder to manage your dotfiles with git\n"
 }
 
@@ -80,7 +81,8 @@ list() {
 install() {
   local dotfiles exclude
   local install_cmd='cp -i'
-  local ret=1
+  local plugins=0
+  local ret=0
 
   # parse the positional parameters
   while true; do
@@ -89,13 +91,16 @@ install() {
         usage install
         exit 2;;
       -e|--exclude )
-        if [ "${dotfiles}" ] || [ "${exclude}" ]; then
+        if [ "${dotfiles}" ] || [ "${exclude}" ] || [ -z $2 ]; then
           usage install
           exit 2
         else
           exclude="${2//,/|}"
         fi
         shift 2;;
+      -p|--plugins )
+        plugins=1
+        shift;;
       -s|--symlink )
         # some busybox's 'ln' miss the '-i' option
         install_cmd='cp -is'
@@ -120,22 +125,60 @@ install() {
   for dir in $(printf "%s" "${dotfiles}"); do
     # if it's an existent directory and it doesn't start with '.'
     if [ -d "${dir}" ] && [[ "${dir}" != '.'* ]] ; then
-      # create the subdirs
-      find "${dir}"/ -type d -exec bash -c \
-        '[[ "${1}" != @(${2}) ]] && mkdir -p "${HOME}/${3#*/}"' \
-        -- "${dir}" "${exclude}" {} \;
-      # install the dotfiles
-      find "${dir}" -type f -exec bash -c \
-        '[[ "${1}" != @(${2}) ]] && ${3} "${PWD}/${4}" "${HOME}/${4#*/}"' \
-        -- "${dir}" "${exclude}" "${install_cmd}" {} \;
-      # if we installed at least one dotfile, return success
-      ret=0
+      # if it's not meant to be ignored
+      if [[ $dir != @($exclude) ]]; then
+        # create the subdirs
+        find "${dir}"/ -type d -exec bash -c \
+          'mkdir -p "${HOME}/${0#*/}"' {} \;
+        # install the dotfile
+        find "${dir}" -type f -exec bash -c \
+          '${0} "${PWD}/${1}" "${HOME}/${1#*/}"' "${install_cmd}" {} \;
+      fi
     elif [ ! -e "${dir}" ]; then
       printf "%s\n" "Inexistent dotfile ${dir}"
     fi
   done
 
-  return "${ret}"
+  if (( $plugins == 1 )); then
+    install_plugins ${dotfile// /,}
+    ret=$?
+  fi
+
+  return $ret
+}
+
+install_plugins() {
+  local dotfiles=$0
+  local ret=0
+
+  for dotfile in $(printf "%s" "${dotfiles//,/ }"); do
+    case $dotfile in
+      neovim )
+        sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+          https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+        nvim -es -u ~/.config/nvim/init.vim <<< 'PlugUpdate --sync'
+        ;;
+      tmux )
+        git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+        git clone https://github.com/tmux-plugins/tmux-resurrect ~/.tmux/plugins/tmux-resurrect
+        git clone https://github.com/tmux-plugins/tmux-continuum ~/.tmux/plugins/tmux-continuum
+        ;;
+      zsh )
+        git clone --depth=1 https://github.com/romkatv/powerlevel10k.git \
+          ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/themes/powerlevel10k
+        git clone https://github.com/Aloxaf/fzf-tab \
+          ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/fzf-tab
+        git clone https://github.com/zsh-users/zsh-autosuggestions \
+          ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+        git clone https://github.com/zdharma/fast-syntax-highlighting.git \
+          ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/fast-syntax-highlighting
+        ;;
+      * )
+        ;;
+    esac
+  done
+
+  return $ret
 }
 
 tui() {
@@ -175,3 +218,4 @@ main() {
 
 cd "${BASH_SOURCE[0]%/*}"
 main "${@}"
+
